16bfa5a40f59630af8afc73ba52392aa9284cee3
Justin Yueh <justin.yueh@gmail.com>
2021-01-17 16:51:19 +0800

    chore: Blog title

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ if (!spaceId || !accessToken) {
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Contentful starter",
+    title: "Justin Yueh's Blog",
   },
   pathPrefix: "/gatsby-contentful-starter",
   plugins: [
